aec73bb18ba288a70158c4b1467edc3074ea9761
Morteza Matbou <mortezamatbou91@gmail.com>
2024-06-27 11:33:18 +0330

    refactor module

--- tennis_data_analysis/data/make_dataset_to_db.py
+++ tennis_data_analysis/data/make_dataset_to_db.py
@@ -2,30 +2,24 @@ import os
 from dotenv import load_dotenv, find_dotenv
 import pandas as pd
 import numpy as np
-import mysql.connector
+import tennis_data_analysis.db.mysql as mysql
 
 
 # find .env automagically by walking up directories until it's found
 dotenv_path = find_dotenv()
-
 # load up the entries as environment variables
 load_dotenv(dotenv_path)
 
-# Insert cleaned data to database
-conn = mysql.connector.connect(
-    host=os.environ.get("DB_HOST"),
-    user=os.environ.get("DB_USERNAME"),
-    password=os.environ.get("DB_PASSWORD"),
-    database=os.environ.get("DB_DATABASE"),
-    port=os.environ.get("DB_PORT")
-)
 
-db = conn.cursor()
+# instance of database object from module
+mysql = mysql.Database(os.environ.get('DB_HOST'), os.environ.get('DB_USERNAME'), os.environ.get('DB_PASSWORD'), os.environ.get('DB_DATABASE'), os.environ.get('DB_PORT'))
 
-
+exit()
 # give a dataframe, generate sql and execute
+
+
 def df_to_table(table_name: str, df: pd.DataFrame):
-    db.execute(f"TRUNCATE TABLE {table_name}")
+    mysql.db.execute(f"TRUNCATE TABLE {table_name}")
 
     columns = ",".join(df.columns.to_list())
     holders = ",".join(["%s"] * df.columns.size)
@@ -33,17 +27,17 @@ def df_to_table(table_name: str, df: pd.DataFrame):
     for index, row in df.iterrows():
         values = tuple(row.values)
         try:
-            db.execute(sql, values)
+            mysql.db.execute(sql, values)
         except Exception as error:
             print(error)
             print(row)
             exit()
 
-    conn.commit()
+    mysql.commit()
 
 
 def votes_to_table(table_name: str, df: pd.DataFrame):
-    db.execute(f"TRUNCATE TABLE {table_name}")
+    mysql.db.execute(f"TRUNCATE TABLE {table_name}")
 
     columns = ",".join(df.columns.to_list())
     holders = ",".join(["%s"] * df.columns.size)
@@ -51,13 +45,13 @@ def votes_to_table(table_name: str, df: pd.DataFrame):
     for index, row in df.iterrows():
         values = tuple(int(v) for v in row)
         try:
-            db.execute(sql, values)
+            mysql.db.execute(sql, values)
         except Exception as error:
             print(error)
             print(row)
             exit()
 
-    conn.commit()
+    mysql.commit()
 
 
 V1_DATA_RAW_DIR = os.path.abspath('./data/raw/01')
