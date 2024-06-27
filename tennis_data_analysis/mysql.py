1c738dbe10166dc55810ce2a7362448a145991e5
Morteza Matbou <mortezamatbou91@gmail.com>
2024-06-27 11:19:46 +0330

    database connection with class

---
+++ tennis_data_analysis/mysql.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+import mysql.connector
+
+
+class Database:
+
+    def __init__(self, host, user, password, dbname, port) -> None:
+        try:
+            self.__conn = mysql.connector.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=dbname,
+                port=port
+            )
+            self.db = self.__conn.cursor()
+        except Exception as error:
+            print("Database connection error")
+            print(error)
+            exit()
+
+    def conn(self) -> mysql.connector.connection_cext.CMySQLConnection:
+        return self.__conn
+
+    def commit(self) -> None:
+        self.__conn.commit()
